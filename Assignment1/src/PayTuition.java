d88d09dbcb21a531ab7c70bf3a1e84cf044f6747
Jim_Pai <Jim_Pai@10.1.10.30>
2016-09-12 23:56:12 -0700

    Modify Scenario 1.

--- Assignment1/src/PayTuition.java
+++ Assignment1/src/PayTuition.java
@@ -1,9 +1,61 @@
+/*
 
-public class Test {
+Scenario : Pay tuition
+Description : A student wants to pay his tuition through myNEU account.
+
+Objects (with States and behaviors) :
+	Object : Student
+		State : name, username, password
+		Behavior : loginMyNEU(), makePaymentOrder(), transferMoneyToNEU()
+
+	Object : myNEUWebsite
+		State : NUID, student's name
+		Behavior : GetStudentInfo(), displayAmountOfTuition(), acceptMoneyFromStudent()
+
+	Object : Money
+		State : amount
+
+*/
+
+public class PayTuition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Student jim = new Student();
+		jim.name = "Jim";
+		jim.username = "xxxx";
+		jim.password = "yyyy";
+		
+		myNEUWebsite myneu = new myNEUWebsite();
+		jim.loginMyNEU(myneu);
+		myneu.getStudentInfo(jim.username);
+		myneu.displayAmountOfTuition();
+		jim.makePaymentOrder();
+		Money money = jim.transferMoneyToNEU(myneu);
+		myneu.acceptMoneyFromStudent(jim, money);
 	}
 
 }
+
+class Student{
+	String name;
+	String username;
+	String password;
+	void loginMyNEU(myNEUWebsite neu){}
+	void makePaymentOrder(){}
+	Money transferMoneyToNEU(myNEUWebsite neu){Money m = new Money(); return m;}
+	
+}
+
+class myNEUWebsite{
+	String NUID;
+	String studentName;
+	void getStudentInfo(String username){}
+	void displayAmountOfTuition(){}
+	void acceptMoneyFromStudent(Student s, Money money){}
+	
+}
+
+class Money{
+	float Amount;
+}
+
